8d27b030fb8deac5b15554e943e647cb6aa2b1ab
Andres Orozco <andres.orozco97@hotmail.com>
2022-12-04 15:00:19 -0500

    create sidebar

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // JS
+
+const handleSidebarClicked = (sidebarName) => {
+    const section = document.getElementById(sidebarName);
+    if (section.style.display === "none" || section.style.display === "") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
